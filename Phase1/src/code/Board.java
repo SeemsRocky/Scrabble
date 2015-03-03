c90c377714926a7f945711d394a7d90b76956e2b
Adhish <Adhish@192.168.1.4>
2015-03-03 00:33:32 -0500

    Added code for the Tile class. Edited somethings in the Board class.

--- Phase1/src/code/Board.java
+++ Phase1/src/code/Board.java
@@ -5,16 +5,19 @@ public class Board
 	private Tile[][] _Board;
 	public Board()
 	{
-		_Board = Tile[20][20];
+		//added 'new'
+		_Board = new Tile[20][20];
 	}
 	//place tile from x,y
 	public void place(Tile t,int x, int y) 
 	{
-		Tile[x][y] = t;
+		//Changed from Tile[x][y] to _Board[x][y]
+		_Board[x][y] = t;
 	}
 	//remove tile from x,y
 	public void remove(Tile t,int x, int y)
 	{
-		Tile[x][y] = null;
+		//Changed from Tile[x][y] to _Board[x][y]
+		_Board[x][y] = null;
 	}
 }
