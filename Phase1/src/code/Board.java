348a162635d7194abe694f0c473278fdbb3469c3
rockylia <rockylia@dragonforce.cse.buffalo.edu>
2015-03-02 22:53:35 -0500

    not done yet~

--- Phase1/src/code/Board.java
+++ Phase1/src/code/Board.java
@@ -1,5 +1,20 @@
 package code;
 
-public class Board {
-
+public class Board 
+{
+	private Tile[][] _Board;
+	public Board()
+	{
+		_Board = Tile[20][20];
+	}
+	//place tile from x,y
+	public void place(Tile t,int x, int y) 
+	{
+		Tile[x][y] = t;
+	}
+	//remove tile from x,y
+	public void remove(Tile t,int x, int y)
+	{
+		Tile[x][y] = null;
+	}
 }
