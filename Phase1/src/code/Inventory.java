7b587060a1b99bc73446060b1d345c4e5335abce
Adhish <Adhish@dhcp035-166-240.wireless.buffalo.edu>
2015-03-03 18:00:55 -0500

    Changes to Inventory class. Shifted from Bag implementation to ArrayList implementation. Added a package tests. Added InventoryTests. Does not test anything except the size of the Inventory as of now.

--- Phase1/src/code/Inventory.java
+++ Phase1/src/code/Inventory.java
@@ -1,17 +1,36 @@
 package code;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Inventory {
-
+    private ArrayList<Tile> _myInventory;
 	public Inventory(){
-		ArrayList<Tile> myInventory = new ArrayList<Tile>();
+		_myInventory = new ArrayList<Tile>();
 		for(int i = 0; i < 29; i++){
 		String vowels = "AEIOU";
 		for(int l = 0; l<vowels.length(); l++){
 			char c = vowels.charAt(l);
-			myInventory.add(new Tile(c));
+			_myInventory.add(new Tile(c));
 		}
-		}	
+		}
+		for(int i = 0; i<15; i++){
+			_myInventory.add(new Tile('Y'));
+		}
+		for(int i = 0; i<12; i++){
+			String rest = "BCDFGHJKLMNPQRSTVWXZ";
+			for(int l = 0; l<rest.length(); l++){
+				char c = rest.charAt(l);
+				_myInventory.add(new Tile(c));
+			}
+			
+		}
+		Collections.shuffle(_myInventory);
+	}
+	public Tile getTile(int i){
+		return _myInventory.get(i);
+	}
+	public int getSize(){
+		return _myInventory.size();
 	}
 }
---
+++ Phase1/src/tests/InventoryTests.java
@@ -0,0 +1,19 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import code.Inventory;
+
+public class InventoryTests {
+
+	@Test
+	public void test() {
+		Inventory i = new Inventory();
+		System.out.println(i.getSize());
+		
+		
+	}
+
+}
