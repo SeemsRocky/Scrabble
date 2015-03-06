bc36e9558d19c5c6b36d4be64d4821f31972ed6e
Adhish <Adhish@dhcp035-165-011.wireless.buffalo.edu>
2015-03-06 16:37:17 -0500

    Added getTile test.

--- Phase1/src/tests/TileRackTest.java
+++ Phase1/src/tests/TileRackTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import code.Inventory;
+import code.Tile;
 import code.TileRack;
 
 public class TileRackTest {
@@ -33,7 +34,7 @@ public class TileRackTest {
 		assertTrue("", actual == expected);
 	}
 	@Test
-	public void newtilerackandrefilltest() {
+	public void newTileRackAndRefillTest() {
 		Inventory inv = new Inventory();
 		TileRack tr = new TileRack(inv);
 		tr.getSize();
@@ -42,12 +43,17 @@ public class TileRackTest {
 		assertTrue("", actual == expected);
 	}
 	@Test
-	public void gettiletest() {
+	public void getTileTest() {
 		Inventory inv = new Inventory();
 		TileRack tr = new TileRack(inv);
-		tr.getSize();
-		int actual = tr.getSize();
-		int expected = 12;
+		for(int i = 0; i<12; i++){
+			tr.remove(0);
+		}
+		tr.addTile(new Tile('A'));
+		char expected = 'A'; 
+		Tile t =	tr.getTile(0);
+	    char actual = t.getLetter();
+		
 		assertTrue("", actual == expected);
 	}
 
