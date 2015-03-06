36aa3e78c22c9ff921c0f99c2f5b01e231cf3eef
Jonathan <Jonathan@Jonathan-PC>
2015-03-06 17:41:16 -0500

    asdasd

--- Phase1/src/tests/InventoryTests.java
+++ Phase1/src/tests/InventoryTests.java
@@ -7,50 +7,65 @@ import org.junit.Test;
 import code.Inventory;
 import code.TileRack;
 
-
 public class InventoryTests {
 
 	@Test
-	public void sizeOfInventory() {
+	public void sizeOfInventory() { // This test confirms that a new inventory
+									// will have a size of 400.
 		Inventory i = new Inventory();
 		int expected = 400;
 		int actual = i.getSize();
-		assertTrue("The expected value was " + expected + " but the value I got was " + actual, actual==expected);
+		assertTrue("The expected value was " + expected
+				+ " but the value I got was " + actual, actual == expected);
 	}
-	
+
 	@Test
-	public void sizeAfterCreatingTileRack() {
+	public void sizeAfterCreatingTileRack() { // This test confirms that a new
+												// TileRack will take 12 Tiles
+												// from the Inventory.
 		Inventory i = new Inventory();
 		TileRack t = new TileRack(i);
-     	int actual = i.getSize();
+		int actual = i.getSize();
 		int expected = 388;
-		System.out.println("The size of the inventory is "+actual);
-		assertTrue( "the expected is " + expected 
-				+ "the actual is " + actual,
-				expected==actual);
+		System.out.println("The size of the inventory is " + actual);
+		assertTrue("the expected is " + expected + "the actual is " + actual,
+				expected == actual);
 	}
-	
+
 	@Test
-	public void sizeAfterRemovingTiles() {
+	public void sizeAfterRemovingTiles() {// This test creates an inventory and
+											// tilerack, then removes the tile
+											// from the tilerack at position 0.
+											// Then the refillIfNotFull method
+											// refills the TileRack, using one
+											// extra tile from the inventory
+											// than the previous test.
 		Inventory i = new Inventory();
 		TileRack t = new TileRack(i);
 		t.remove(0);
 		t.refillIfNotFull();
 		int expected = 387;
 		int actual = i.getSize();
-		assertTrue("The expected value was " + expected + " but the value I got was " + actual, actual==expected);
-		
+		assertTrue("The expected value was " + expected
+				+ " but the value I got was " + actual, actual == expected);
+
 	}
+
 	@Test
-	public void sizeAfterCreatingMultipleTileRacks() {
+	public void sizeAfterCreatingMultipleTileRacks() {// This test confirms that
+														// each Tilerack made
+														// will always take 12
+														// tiles from the
+														// inventory.
 		Inventory i = new Inventory();
 		TileRack t = new TileRack(i);
 		TileRack t1 = new TileRack(i);
 		TileRack t2 = new TileRack(i);
-		
+
 		int expected = 364;
 		int actual = i.getSize();
-		assertTrue("The expected value was " + expected + " but the value I got was " + actual, actual==expected);
-		
+		assertTrue("The expected value was " + expected
+				+ " but the value I got was " + actual, actual == expected);
+
 	}
 }
--- Phase1/src/tests/TileTests.java
+++ Phase1/src/tests/TileTests.java
@@ -9,31 +9,40 @@ import code.Tile;
 public class TileTests {
 
 	@Test
-	public void getTileTest1() {
+	public void getTileTest1() { // This test confirms that the getLetter method
+									// is working correctly
 		Tile t = new Tile('A');
 		char actual = t.getLetter();
 		char expected = 'A';
-		assertTrue("The expected value was " + expected + " but the value I got was " + actual, actual==expected);
+		assertTrue("The expected value was " + expected
+				+ " but the value I got was " + actual, actual == expected);
 	}
-	
+
 	@Test
-	public void getValueTest() {
+	public void getValueTest() { // The next 3 tests confirm that the getValue
+									// method returns the correct amount of
+									// points for each type of character.
 		Tile t = new Tile('A');
 		int actual = t.getValue();
 		int expected = 1;
-		assertTrue("The expected value was " + expected + " but the value I got was " + actual, actual==expected);		
+		assertTrue("The expected value was " + expected
+				+ " but the value I got was " + actual, actual == expected);
 	}
+
 	public void getValueTest2() {
 		Tile t = new Tile('Y');
 		int actual = t.getValue();
 		int expected = 2;
-		assertTrue("The expected value was " + expected + " but the value I got was " + actual, actual==expected);		
+		assertTrue("The expected value was " + expected
+				+ " but the value I got was " + actual, actual == expected);
 	}
+
 	public void getValueTest3() {
 		Tile t = new Tile('Z');
 		int actual = t.getValue();
 		int expected = 5;
-		assertTrue("The expected value was " + expected + " but the value I got was " + actual, actual==expected);		
+		assertTrue("The expected value was " + expected
+				+ " but the value I got was " + actual, actual == expected);
 	}
 
 }
