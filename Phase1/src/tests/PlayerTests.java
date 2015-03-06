fee05c3fdd804a6ad17944956246ec4e9e29d814
jhe8 <jhe8@nickelback.cse.buffalo.edu>
2015-03-06 18:13:57 -0500

    d

--- Phase1/src/tests/PlayerTests.java
+++ Phase1/src/tests/PlayerTests.java
@@ -13,13 +13,18 @@ import code.TileRack;
 public class PlayerTests {
 
 	@Test
-	public void test() {
+	public void getScoreTest() {// This test confirms that the getScore method
+								// is
+		// functioning correctly. A TileRack is created but we
+		// removed the random tiles so that we can have Tiles
+		// that can be made into a word. The values of the
+		// letters D, O, and G are added up.
 		Inventory inv = new Inventory();
 		Board b = new Board();
 		Player p1 = new Player("Adhish", b, inv);
 		TileRack t = new TileRack(inv);
 		p1.setTileRack(t);
-		for(int i = 0; i<12; i++){
+		for (int i = 0; i < 12; i++) {
 			t.remove(0);
 		}
 		t.addTile(new Tile('D'));
@@ -32,8 +37,20 @@ public class PlayerTests {
 		p1.moveComplete();
 		int expected = 11;
 		int actual = p1.getScore();
-		
-		assertTrue("The expected value was " + expected + " but the value I got was " + actual, actual==expected);
+
+		assertTrue("The expected value was " + expected
+				+ " but the value I got was " + actual, actual == expected);
 	}
 
+	@Test
+	public void getNameTest() {
+		Inventory inv = new Inventory();
+		Board b = new Board();
+		Player p1 = new Player("Adhish", b, inv);
+		String expected = "Adhish";
+		String actual = p1.getName();
+		assertTrue("The expected value was " + expected
+				+ " but the value I got was " + actual, actual == expected);
+
+	}
 }
