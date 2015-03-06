7837b8acd8a5990edd85ea77caf3c4d6ab60e9bd
Adhish <Adhish@dhcp035-165-011.wireless.buffalo.edu>
2015-03-06 15:27:17 -0500

    esrgf

--- Phase1/src/code/Player.java
+++ Phase1/src/code/Player.java
@@ -9,28 +9,46 @@ public class Player {
     private int _score;
     private TileRack _myTileRack;
     private ArrayList<Tile> _TilesMovedByPlayer;
+    
+    
+    /**
+     * The constructor sets up the player with a name, the existing board and the inventory. It also
+     * creates a TileRack for the player and an ArrayList of tiles which keeps track of the tiles moved by 
+     * a player in a single turn.
+     * @param name A string representing a player's name.
+     * @param b Board of the game.
+     * @param inv Inventory of the game. 
+     */
     public Player(String name, Board b, Inventory inv){
-    	//Unique name for every player
     	_name = name;
-    	//Board common for all
     	_board = b;
-    	//Inventory common for all
     	_inventory = inv;
-    	//Creates a new TileRack for a player
     	_myTileRack = new TileRack(_inventory);
     	_TilesMovedByPlayer = new ArrayList<Tile>();
     }
+    /**
+     * This method returns the current score of the player.
+     * 
+     * @return An int representing the player's score.
+     */
     public int getScore(){
-    	//returns the score. not yet implemented on how to calculate the score
     	return _score;
     }
+    /**
+     * This method places a tile from the TileRack of the player to the board. It checks whether the board
+     * is empty at that coordinate before putting the tile there. After placing the tile on the board, it adds
+     * the tile to an ArrayList that keeps track of the tiles moved by a player in a single turn. Finally, the
+     * tile is removed from the player's TileRack. 
+     * @param indexTileRack An int representing the index of the TileRack.
+     * @param xcoordinate An int representing the x-coordinate of the board.
+     * @param ycoordinate 
+     */
     public void placeATileOnTheBoard(int indexTileRack, int xcoordinate, int ycoordinate){
-    	//index of the rack from 0 to 11 (Selects a particular letter)
-    	// xcoordinate and ycoordinate are the positions on the board
+    	if(_board.isEmpty(xcoordinate, ycoordinate)){
     	_board.place(_myTileRack.getTile(indexTileRack), xcoordinate , ycoordinate);
     	_TilesMovedByPlayer.add(_myTileRack.getTile(indexTileRack));
     	_myTileRack.remove(indexTileRack);
-    	
+    	}
    	 
     }
     public void moveComplete(){
