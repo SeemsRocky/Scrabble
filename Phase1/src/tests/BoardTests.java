062e19b7c75d1857f7872fbb7417152f49b53280
jhe8 <jhe8@luciano.cse.buffalo.edu>
2015-03-06 19:20:47 -0500

    dddd

--- Phase1/src/tests/BoardTests.java
+++ Phase1/src/tests/BoardTests.java
@@ -9,7 +9,7 @@ import code.Tile;
 public class BoardTests 
 {
 	@Test
-	public void getTileTest() //This test confirms the functionality of the getTileAt test 
+	public void getTileTest() //This test confirms the functionality of the getTileAt test. An 'A' tile is placed on the board.
 	{
 		Tile t = new Tile('A');
 		Board b = new Board();
@@ -20,7 +20,7 @@ public class BoardTests
 				"the expected tile was " +expected, actual == expected);
 	}
 	@Test
-	public void removeTileTest() 
+	public void removeTileTest() //This test confirms that the removeTile method works correctly. An 'A' tile is placed on the board and is then removed.
 	{
 		Tile t = new Tile('A');
 		Board b = new Board();
@@ -32,7 +32,7 @@ public class BoardTests
 				"the expected tile was " +expected, actual == expected);
 	}
 	@Test
-	public void checkEmptyTest() 
+	public void checkEmptyTest() //This test confirms that the checkEmpty method works correctly. A new board does not contain any Tiles.
 	{
 		Board b = new Board();
 		boolean expected = true ;
@@ -41,7 +41,7 @@ public class BoardTests
 				"the expected tile was " +expected, actual == expected);
 	}
 	@Test
-	public void checkEmptyTest2() 
+	public void checkEmptyTest2() //This test confirms that the checkEmpty method will return false when there is a tile in that specific position on the board.
 	{
 		Tile t = new Tile('R');
 		Board b = new Board();
