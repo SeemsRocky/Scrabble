ef972542cf61aa7b1b940e4ba7c238f004f94547
Adhish <Adhish@dhcp035-167-115.wireless.buffalo.edu>
2015-04-04 18:31:13 -0400

    Updated ScrabbleWindow.

--- Phase 2 code/src/code/ui/ScrabbleWindow.java
+++ Phase 2 code/src/code/ui/ScrabbleWindow.java
@@ -2,6 +2,7 @@ package code.ui;
 import java.awt.*;
 import java.util.*;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -25,18 +26,31 @@ public class ScrabbleWindow implements Observer, Runnable{
 	@Override
 	public void run() {
 		JFrame window = new JFrame("Scrabble");
-		JPanel p = new JPanel();
-		p.setLayout(new GridLayout(20, 20, 0, 0));
-		for (int i = 0; i < 400; i++) {
-			JButton b = new JButton("Letter");
-			p.add(b);
-			
-		}
-		
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.add(p);
-		window.pack();
-		window.setVisible(true);
+        window.setBounds(10, 10, 2000, 1000);
+         
+        JPanel container = new JPanel();
+        container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
+        JPanel p = new JPanel();
+        p.setSize(40, 40);
+        p.setLayout(new GridLayout(20, 20, 0, 0));
+         
+        for (int i = 0; i < 400; i++) {
+            JButton b = new JButton("Letter");
+            p.add(b);
+             
+        }
+        JPanel q = new JPanel();
+     
+         
+ 
+         
+         
+        container.add(p);
+        container.add(q);
+         
+        window.add(container);
+        window.setVisible(true);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 	}
 
