8321a07e8a072842dcdc553b2223af9435349b08
Adhish <Adhish@dhcp035-167-115.wireless.buffalo.edu>
2015-04-03 21:40:49 -0400

    SaveTests file is commented out for now.

--- Phase 2 code/src/tests/SaveTests.java
+++ Phase 2 code/src/tests/SaveTests.java
@@ -1,22 +1,22 @@
-package tests;
-
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
-import code.model.Board;
-import code.model.Inventory;
-import code.model.Save;
-
-public class SaveTests {
-
-	@Test
-	public void test() {
-		Board b = new Board();
-		Inventory i = new Inventory();
-		Save s = new Save(b, i);
-		s.saveToFile("src/code/model/save.txt");
-		
-	}
-
-}
+//package tests;
+//
+//import static org.junit.Assert.*;
+//
+//import org.junit.Test;
+//
+//import code.model.Board;
+//import code.model.Inventory;
+//import code.model.Save;
+//
+//public class SaveTests {
+//
+//	@Test
+//	public void test() {
+//		Board b = new Board();
+//		Inventory i = new Inventory();
+//		Save s = new Save(b, i);
+//		s.saveToFile("src/code/model/save.txt");
+//		
+//	}
+//
+//}
