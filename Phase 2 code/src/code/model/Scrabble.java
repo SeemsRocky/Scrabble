36ee36bd507e778b6fcce1c856897a9f851810ba
rockylia <rockylia@dragonforce.cse.buffalo.edu>
2015-04-06 14:57:14 -0400

    Stuff that doesn't work i need to fix it up i think the code is so dumb

--- Phase 2 code/src/code/model/Scrabble.java
+++ Phase 2 code/src/code/model/Scrabble.java
@@ -1,10 +1,12 @@
 package code.model;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Scrabble extends Observable{
 
@@ -33,7 +35,10 @@ public class Scrabble extends Observable{
 		_board = new Board();
 		_players = new ArrayList<Player>();
 	}
-
+	public ArrayList<Player> getPlayers()
+	{
+		return _players;
+	}
 	/**
 	 * Adds a new player to the game.
 	 */
@@ -74,4 +79,139 @@ public class Scrabble extends Observable{
 			stream.close();
 		}
 	}
+	/*
+	 * ROCKY'S CODE THAT DOESNT WORK AT ALL 
+	 */
+	public void fileWriteHighScore(Scrabble s, String filename) {
+        PrintStream stream = null;
+        try
+        {
+            stream = new PrintStream(filename);
+            int numLines = getNumberOfLines(filename);
+            ArrayList<String> al = getHighScoreFile(filename);
+            ArrayList<Player> p = s.getPlayers();
+            sort1(p);
+            for(int i=0;i<_players.size();i++)
+            {
+            	if(numLines<20)
+            	{
+            		stream.format(" <\"%s\"> : <%d> %n",p.get(i).getName(),p.get(i).getScore());
+            	}
+            	else
+            	{
+            		for(int j=0;j<p.size();j++)
+            		{
+            			for(int n=0;n<al.size();n++)
+	            		{
+	            			if()
+	            		}
+            		}
+            	}
+            }
+        } catch (FileNotFoundException e)
+        {
+            System.err.println("File not found and cannot be created: "+ filename);
+        }
+        finally
+        {
+            stream.close();
+        }
+    }
+	private void sort1(ArrayList<Player> al)
+	{
+		int pos =0;
+		for(int i =0; i<al.size()-1;i++)
+		{
+			pos=i;
+			for(int j=i+1; j<al.size();j++)
+			{
+				if(al.get(j).getScore()<al.get(i).getScore())
+				{
+					pos = j;
+				}
+			}
+			Player temp = al.get(pos);
+			al.set(pos,al.get(i));
+			al.set(i, temp);
+		}	
+	}
+	private void sort2(ArrayList<String> as)
+	{
+		int pos =0;
+		for(int i =0; i<as.size()-1;i++)
+		{
+			pos=i;
+			for(int j=i+1; j<as.size();j++)
+			{
+				if(convertInt(as.get(j))<convertInt(as.get(i)))
+				{
+					pos = i;
+				}
+			}
+			String temp = as.get(pos);
+			as.set(pos,as.get(i));
+			as.set(i, temp);
+		}	
+	}
+	private int convertInt(String s)
+	{
+		int num=0;
+		int score=0;
+		String scoreString ="";
+		for(int i=0;i<s.length();i++)
+		{
+			if (s.charAt(i)== 32)
+			{
+				num=i;
+			}
+		}
+		for(int j=num+1;j<s.length();j++)
+		{
+			
+			scoreString = scoreString + s.charAt(j);
+		}
+		score = Integer.parseInt(scoreString);
+		return score;
+	}
+	private int getNumberOfLines(String filename)
+    {
+        Scanner scan = new Scanner(filename);
+        int lineNumber = 0;
+        try {
+            scan = new Scanner(new File(filename));
+            while (scan.hasNextLine()) 
+            {
+            	lineNumber++;
+            }
+         } catch (FileNotFoundException e) {
+            System.err.println("File not found: "+filename);
+        }
+        finally {
+            scan.close();
+        }
+        return lineNumber;
+    }
+	public ArrayList<String> getHighScoreFile(String filename)
+    {
+        Scanner scan = null;
+        ArrayList<String> str = new ArrayList<String>();
+        try {
+            scan = new Scanner(new File("src/code/model/Highscores.txt"));
+            int lineNumber = 0;
+            while (scan.hasNextLine()) {
+                String oneLineFromFile = scan.nextLine();
+                str.add(oneLineFromFile);
+                System.out.format("%3d %s %n", lineNumber, oneLineFromFile);
+                lineNumber++;
+            }
+         } catch (FileNotFoundException e) {
+            System.err.println("File not found: "+filename);
+        }
+        finally { 
+            scan.close();
+        }
+        return str;
+    }
+	
+	
 }
