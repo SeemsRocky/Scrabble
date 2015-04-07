669c240dee5e902cdb4d1a12d1c05d873c0e5386
Adhish <Adhish@dhcp205-075-012.wireless.buffalo.edu>
2015-04-07 15:29:32 -0400

    Updated Scrabble file.

--- Phase 2 code/src/code/model/Scrabble.java
+++ Phase 2 code/src/code/model/Scrabble.java
@@ -25,21 +25,39 @@ public class Scrabble extends Observable{
 	 */
 	private int _numberOfPlayers;
 
+	private String _pathOfDictionary;
+	private String _pathOfRestoreFile;
+	private ArrayList<String> _nameOfPlayers;
+
 	/**
 	 * Class constructor
 	 */
+	public Scrabble(int n, ArrayList<String> players){
+		_inv = new Inventory();
+		_board = new Board();
+		for(int i = 0; i<players.size(); i++){
+			addNewPlayer(_inv, players.get(i));
+		}
+
+
+	}
+	
 	public Scrabble(){
 		_inv = new Inventory();
 		_board = new Board();
-
+		for(int i = 1; i<3; i++){
+			addNewPlayer(_inv, "Player " + i);
+		}
 	}
 
 	/**
 	 * Adds a new player to the game.
 	 */
-	//	private void addNewPlayer(){
-	//		_players.add(new Player(_inv));
-	//	}
+	private void addNewPlayer(Inventory inv,String name){
+		_players.add(new Player(inv, name));
+	}
+
+
 	public void saveToFile(String filename) {
 		PrintStream stream = null;
 		try {
@@ -48,7 +66,7 @@ public class Scrabble extends Observable{
 			int widthOfTheBoard = _board.getWidth();
 			String lengthOfBoard = Integer.toString(lengthOfTheBoard);
 			String widthOfBoard = Integer.toString(widthOfTheBoard);
-			String filePathOfDictionary = "\nsrc/code/model/Dictionary.txt";
+			String filePathOfDictionary = _pathOfDictionary;
 			stream.format(lengthOfBoard);
 			stream.format(" " +widthOfBoard);
 			stream.format(filePathOfDictionary);
@@ -73,5 +91,40 @@ public class Scrabble extends Observable{
 		finally {
 			stream.close();
 		}
+	}
+
+
+	public void restoreFromFile(String path){
+            // String[] strings = string.replace("[", "").replace("]", "").split(", ");
+		    // going to need to set up the tile rack
+	}
+
+	public void main(String[] args){
+		if(args.length==1){
+			_pathOfRestoreFile = args[0];
+			this.restoreFromFile(_pathOfRestoreFile);
+		}
+		else{
+			_pathOfDictionary = args[0];
+			_numberOfPlayers = Integer.parseInt(args[1]);
+			String nameOfPlayers = args[2];
+			String word = "";
+			ArrayList<String> arrayOfWords = new ArrayList<String>();
+			for(int i = 0; i<nameOfPlayers.length(); i++){
+				char c = nameOfPlayers.charAt(i);
+				if(c=='\n' || c=='\t' || c == ' '){
+					arrayOfWords.add(word);
+					word = "";
+				}
+				else{
+					word = word + c;
+				}
+			}
+			_nameOfPlayers = arrayOfWords;
+			new Scrabble(_numberOfPlayers, _nameOfPlayers);
+
+		}
+
+
 	}
 }
