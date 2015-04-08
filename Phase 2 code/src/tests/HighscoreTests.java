0a3ff51fdf1deb3af13c8bab2b077602d419a383
rockylia <rockylia@dragonforce.cse.buffalo.edu>
2015-04-07 21:13:54 -0400

    added highscore tests java file

---
+++ Phase 2 code/src/tests/HighscoreTests.java
@@ -0,0 +1,16 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import code.model.Board;
+import code.model.Inventory;
+
+public class HighscoreTests 
+{
+	/*@Test
+	public void test1() 
+	{
+
+	}*/
+}
