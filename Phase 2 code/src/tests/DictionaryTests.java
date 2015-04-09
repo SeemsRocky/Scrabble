c37bf9d032bf4716cca88728e118cfed4d1eb08b
Jonathan <Jonathan@128.205.208.181>
2015-04-08 22:54:49 -0400

    hi

---
+++ Phase 2 code/src/tests/DictionaryTests.java
@@ -0,0 +1,25 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+
+public class DictionaryTests {
+	
+	private void commonTestCode(String input, Boolean expected) {
+		code.model.Dictionary su = new code.model.Dictionary(input);
+		Boolean actual = su.isAWord(input);
+		assertTrue("With input "+input
+				+ " I expected the answer to be " + expected
+				+ " but the actual response I got was "+ actual,
+				expected==actual);
+	}
+
+	@Test public void test01() { commonTestCode("aardvark",true); }
+	@Test public void test02() { commonTestCode("ABC", true); }
+	@Test public void test03() { commonTestCode("accuracy", true); }
+	@Test public void test04() { commonTestCode("dasdasdasdasdasdasdasdsa", false); }
+
+}
