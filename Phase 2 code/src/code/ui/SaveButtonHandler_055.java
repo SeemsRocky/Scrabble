1d2b15a2cc963b5c7affca011fd0d1f3db0265fe
rockylia <rockylia@dragonforce.cse.buffalo.edu>
2015-04-10 19:27:10 -0400

    more javadocs

--- Phase 2 code/src/code/ui/SaveButtonHandler_055.java
+++ Phase 2 code/src/code/ui/SaveButtonHandler_055.java
@@ -10,12 +10,20 @@ import java.awt.event.ActionListener;
 import code.model.Scrabble_055;
 
 public class SaveButtonHandler_055 implements ActionListener {
+	/**
+	 * Scrabble game
+	 */
 	private Scrabble_055 _s;
+	/**
+	 * constructor for the savebuttonhandler
+	 * @param s scrabble game
+	 */
 	public SaveButtonHandler_055(Scrabble_055 s){
 		_s = s;
 	}
 	/**
-	 * @author    driver:
+	 * When button is pressed, it saves to the file "src/code/model/save.txt"
+	 * @author    driver:adhishch (Adhish Chugh)
      * @author navigator: 
 	 */
 
