ceb0bb14f19a8b6ec06d14ce16a369f9c63445ca
Adhish <Adhish@192.168.1.4>
2015-04-10 19:32:11 -0400

    New UI improvements.

--- Phase 2 code/src/code/ui/SubmitButtonHandler_055.java
+++ Phase 2 code/src/code/ui/SubmitButtonHandler_055.java
@@ -1,5 +1,19 @@
 package code.ui;
 
-public class SubmitButtonHandler_055 {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SubmitButtonHandler_055 implements ActionListener {
+	private ScrabbleUI_055 _s;
+	public SubmitButtonHandler_055(ScrabbleUI_055 s){
+		_s =s;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		_s.getCurrentPlayer().moveComplete();
+		_s.moveComplete();
+	}
 
 }
