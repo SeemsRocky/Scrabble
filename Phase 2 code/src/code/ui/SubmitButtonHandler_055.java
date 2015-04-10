ce7cea02b03c9881096bfc11026c712c5c4e72fa
rockylia <rockylia@dragonforce.cse.buffalo.edu>
2015-04-10 19:57:06 -0400

    Javadocs dood

--- Phase 2 code/src/code/ui/SubmitButtonHandler_055.java
+++ Phase 2 code/src/code/ui/SubmitButtonHandler_055.java
@@ -4,11 +4,23 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class SubmitButtonHandler_055 implements ActionListener {
+	/**
+	 *  scrabble game UI
+	 */
 	private ScrabbleUI_055 _s;
+	/**
+	 * constructor for submitbuttonhandler
+	 * @param s scrabble game UI
+	 * * @author    driver:
+     * @author navigator: 
+	 */
 	public SubmitButtonHandler_055(ScrabbleUI_055 s){
 		_s =s;
 	}
-
+	/**
+	 * completes/ends the player's move
+	 * @param e
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
